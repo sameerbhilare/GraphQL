6202e69e65dcc36f57fabc3b07b3a132bd2afb6b
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-29 16:16:53 +0530

    Object Associations: Job/Company

--- Workspace/02-job-board/server/resolvers.js
+++ Workspace/02-job-board/server/resolvers.js
@@ -1,7 +1,13 @@
 const db = require('./db');
+
 const Query = {
   // graphql will return only requested fields, we don't have to do anything special for that.
   jobs: () => db.jobs.list(),
 };
 
-module.exports = { Query };
+const Job = {
+  // each resolver function receives some arguments - the 1st argument is the parent object
+  company: (job) => db.companies.get(job.companyId), // 'job' obj returned fromDB has 'companyId' field (refer jobs.json)
+};
+
+module.exports = { Query, Job };
