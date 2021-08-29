c1609dd66139de62b134b077b7875a54dd40566d
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-29 12:19:06 +0530

    Returning an Array of Jobs

--- Workspace/02-job-board/server/db.js
+++ Workspace/02-job-board/server/db.js
@@ -1,9 +1,17 @@
-const { DataStore } = require('notarealdb');
+const { DataStore } = require('notarealdb'); // package for sample DB
 
-const store = new DataStore('./data');
+const store = new DataStore('./data'); // base folder
 
 module.exports = {
+  // companies will be loaded from .data/companies.json,
+  // then you can execute typical operations on these. e.g. list, insert, delete, udpate, etc.
   companies: store.collection('companies'),
+
+  // jobs will be loaded from .data/jobs.json
+  // then you can execute typical operations on these. e.g. list, insert, delete, udpate, etc.
   jobs: store.collection('jobs'),
-  users: store.collection('users')
+
+  // users will be loaded from .data/users.json
+  // then you can execute typical operations on these. e.g. list, insert, delete, udpate, etc.
+  users: store.collection('users'),
 };
