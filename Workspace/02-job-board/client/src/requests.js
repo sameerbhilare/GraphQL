4a9b6d5fcb9f488bbdb75385091dd82b8223b968
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-29 18:37:21 +0530

    Query Variables: Fetching a Job

--- Workspace/02-job-board/client/src/requests.js
+++ Workspace/02-job-board/client/src/requests.js
@@ -1,5 +1,34 @@
 const endpointUrl = 'http://localhost:9000/graphql';
 
+// fetch single job
+export const loadJob = async (id) => {
+  const response = await fetch(endpointUrl, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: `query JobQuery($id: ID!){
+        job(id: $id) {
+          id
+          title
+          company {
+            id
+            name
+          }
+          description
+        }
+      }
+      `,
+      variables: { id: id },
+    }),
+  });
+
+  const responseData = await response.json();
+  return responseData.data.job;
+};
+
+// fetch all jobs
 export const loadJobs = async () => {
   const response = await fetch(endpointUrl, {
     method: 'POST',
