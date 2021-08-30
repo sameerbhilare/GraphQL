8ebad74c14df21e52bb83b5fb2db4d96338980f1
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-30 12:28:56 +0530

    Authentication with ApolloLink

--- Workspace/02-job-board/client/src/requests.js
+++ Workspace/02-job-board/client/src/requests.js
@@ -1,46 +1,37 @@
-import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost';
+import { ApolloClient, ApolloLink, HttpLink, InMemoryCache } from 'apollo-boost';
 import gql from 'graphql-tag';
 import { getAccessToken, isLoggedIn } from './auth';
 
 const endpointUrl = 'http://localhost:9000/graphql';
 
+// "operation" is the GraphQL query or mutation to be executed
+const authLink = new ApolloLink((operation, forward) => {
+  // Add 'Authorization' header for logged in user
+  // which will be used by the HttpLink when making the actual HTTP request
+  if (isLoggedIn()) {
+    operation.setContext({
+      headers: {
+        Authorization: 'Bearer ' + getAccessToken(),
+      },
+    });
+  }
+  // "forward' is a function that allows us to chain multiple steps together
+  return forward(operation);
+});
+
 // create Apollo Client
 const client = new ApolloClient({
   // how to connect to server
-  link: new HttpLink({ uri: endpointUrl }),
+  link: ApolloLink.from([
+    // add auth link before HttpLink so that 'authLink' will be executed first and then HttpLink
+    authLink,
+    new HttpLink({ uri: endpointUrl }),
+  ]),
 
   // caching is one of main features of Apollo Client
   // there are many implementations of caching storage e.g. InMemoryCache, local storage, Async storage
   cache: new InMemoryCache(),
 });
-
-// calling graphql endpoint (common)
-const graphqlRequest = async (query, variables = {}) => {
-  const request = {
-    method: 'POST',
-    headers: {
-      'content-type': 'application/json',
-    },
-    body: JSON.stringify({
-      query,
-      variables, // varibles (if any) to given query
-    }),
-  };
-
-  // Add 'Authorization' header for logged in user
-  if (isLoggedIn()) {
-    request.headers['Authorization'] = 'Bearer ' + getAccessToken();
-  }
-
-  const response = await fetch(endpointUrl, request);
-
-  const responseData = await response.json();
-  if (responseData.errors) {
-    const message = responseData.errors.map((err) => err.message).join('\n');
-    throw new Error(message);
-  }
-  return responseData.data;
-};
 
 // fetch single job
 export const loadJob = async (id) => {
@@ -128,6 +119,7 @@ export const createJob = async (input) => {
     }
   `;
 
+  // mutation
   const { data } = await client.mutate({ mutation: mutation, variables: { input } });
   return data.job;
 };
