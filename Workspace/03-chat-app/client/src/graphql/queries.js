a81dd1c7c0d663f13cf4d1f925a781761fc50101
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-30 19:21:49 +0530

    Using Subscriptions in the Client

--- Workspace/03-chat-app/client/src/graphql/queries.js
+++ Workspace/03-chat-app/client/src/graphql/queries.js
@@ -21,15 +21,37 @@ const addMessageMutation = gql`
   }
 `;
 
+// subscription
+const messageAddedSubscription = gql`
+  subscription {
+    messageAdded {
+      id
+      from
+      text
+    }
+  }
+`;
+
 export async function addMessage(text) {
-  const {data} = await client.mutate({
+  const { data } = await client.mutate({
     mutation: addMessageMutation,
-    variables: {input: {text}}
+    variables: { input: { text } },
   });
   return data.message;
 }
 
 export async function getMessages() {
-  const {data} = await client.query({query: messagesQuery});
+  const { data } = await client.query({ query: messagesQuery });
   return data.messages;
+}
+
+export function onMessageAdded(handlerMessage) {
+  // This initiates the GraphQL subscription with the server,
+  // which means the server will send messages to this client over WebSocket.
+  const observable = client.subscribe({ query: messageAddedSubscription }); // receive data from server
+
+  // subscribe to ongoing messages between server and client
+  return observable.subscribe(({ data }) => {
+    handlerMessage(data.messageAdded); // dispatch data to other parts of client application
+  });
 }
