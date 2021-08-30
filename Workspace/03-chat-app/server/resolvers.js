0744d60f90410fb2533d494c470b03b7d07825a6
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-30 16:16:35 +0530

    Subscription Resolver with PubSub

--- Workspace/03-chat-app/server/resolvers.js
+++ Workspace/03-chat-app/server/resolvers.js
@@ -1,4 +1,11 @@
+const { PubSub } = require('graphql-subscriptions');
 const db = require('./db');
+
+const MESSAGE_ADDED = 'MESSAGE_ADDED';
+
+// this 'PubSub' is basic PubSub which will work properly on single server.
+// for prod scenarios, you can use alternate pubsub implementations available.
+const pubSub = new PubSub();
 
 function requireAuth(userId) {
   if (!userId) {
@@ -7,18 +14,32 @@ function requireAuth(userId) {
 }
 
 const Query = {
-  messages: (_root, _args, {userId}) => {
+  messages: (_root, _args, { userId }) => {
     requireAuth(userId);
     return db.messages.list();
-  }
-}
+  },
+};
 
 const Mutation = {
-  addMessage: (_root, {input}, {userId}) => {
+  addMessage: (_root, { input }, { userId }) => {
     requireAuth(userId);
-    const messageId = db.messages.create({from: userId, text: input.text});
-    return db.messages.get(messageId);
-  }
-}
+    const messageId = db.messages.create({ from: userId, text: input.text });
+    const message = db.messages.get(messageId);
 
-module.exports = { Query, Mutation };
+    // 1st arg is event name as mentioned in the 'Subscription' asyncIterator.
+    // 2nd arg is obj with property of the same name as the one in 'Subscription'
+    pubSub.publish(MESSAGE_ADDED, { messageAdded: message });
+    return message;
+  },
+};
+
+const Subscription = {
+  messageAdded: {
+    // 'iterator' because with a subscription, the client doesn't receive a single value,
+    // like with a query, but it can receive multiple values over time.
+    // This will take care of notifying all the subscribers every time we publish a new message.
+    subscribe: () => pubSub.asyncIterator(MESSAGE_ADDED),
+  },
+};
+
+module.exports = { Query, Mutation, Subscription };
